e805e19afee01f1986df0afc912c80ce0848a847
WangLei <nkwanglei@126.com>
2016-11-28 17:59:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var body = [
           '</span>',
           '<div style="width:100%;pading:50px 100px;text-align:center;">',
               '<h1>Hello World - v10</h1>',
-              '<p>Welcome to Node app - DevOps 2016-10-05.</span></p>',
+              '<p>Welcome to Node app - DevOps 2016-11-05.</span></p>',
           '</div>',
       '</div>'
    ].join('');
